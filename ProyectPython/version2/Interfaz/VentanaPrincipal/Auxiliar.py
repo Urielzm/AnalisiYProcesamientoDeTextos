63c2c23962bba6d7c9547e30e22849ee2a101b55
unknown <uriel-zagoya-mellado@hotmail.com>
2020-04-18 21:50:50 -0500

    Interfaz version 2

--- ProyectPython/version2/Interfaz/VentanaPrincipal/Auxiliar.py
+++ ProyectPython/version2/Interfaz/VentanaPrincipal/Auxiliar.py
@@ -1,141 +1,66 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'VentanaPrincipal.ui'
+# Form implementation generated from reading ui file 'VentanaMenu.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
 #
 # WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 sys.path.append("..")
 from Dialogos.DialogoAviso.DialogoAvisos import Ui_DialogAviso
+from VentanaPrincipal import Ui_MainWindow
 from Backend.Resumen import Resumen
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(801, 600)
-        MainWindow.setMinimumSize(QtCore.QSize(801, 600))
-        MainWindow.setMaximumSize(QtCore.QSize(801, 600))
-        MainWindow.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"background-color: qlineargradient(spread:pad, x1:0.824, y1:0.198864, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton_Salir = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_Salir.setGeometry(QtCore.QRect(710, 530, 75, 23))
-        self.pushButton_Salir.setStyleSheet("background-color: rgb(157, 157, 157);")
-        self.pushButton_Salir.setObjectName("pushButton_Salir")
-        self.pushButton_Obtemer_Resumen = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_Obtemer_Resumen.setGeometry(QtCore.QRect(700, 490, 81, 23))
-        self.pushButton_Obtemer_Resumen.setStyleSheet("background-color: rgb(154, 154, 154);")
-        self.pushButton_Obtemer_Resumen.setObjectName("pushButton_Obtemer_Resumen")
-        self.label_TextoIntroduce_Texto = QtWidgets.QLabel(self.centralwidget)
-        self.label_TextoIntroduce_Texto.setGeometry(QtCore.QRect(170, 60, 351, 31))
-        self.label_TextoIntroduce_Texto.setObjectName("label_TextoIntroduce_Texto")
-        self.label_Resumen_Obtenido = QtWidgets.QLabel(self.centralwidget)
-        self.label_Resumen_Obtenido.setGeometry(QtCore.QRect(180, 360, 241, 31))
-        self.label_Resumen_Obtenido.setObjectName("label_Resumen_Obtenido")
-        self.textEdit_Entrada_de_texto = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_Entrada_de_texto.setGeometry(QtCore.QRect(170, 90, 441, 91))
-        self.textEdit_Entrada_de_texto.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.textEdit_Entrada_de_texto.setObjectName("textEdit_Entrada_de_texto")
-        self.textEdit_Salida_Resumen_Obtenido = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_Salida_Resumen_Obtenido.setGeometry(QtCore.QRect(180, 400, 441, 151))
-        self.textEdit_Salida_Resumen_Obtenido.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.textEdit_Salida_Resumen_Obtenido.setReadOnly(False)
-        self.textEdit_Salida_Resumen_Obtenido.setObjectName("textEdit_Salida_Resumen_Obtenido")
-        self.textEdit_Salida_Tabla_Frecuencias = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_Salida_Tabla_Frecuencias.setGeometry(QtCore.QRect(70, 230, 311, 131))
-        self.textEdit_Salida_Tabla_Frecuencias.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.textEdit_Salida_Tabla_Frecuencias.setReadOnly(False)
-        self.textEdit_Salida_Tabla_Frecuencias.setObjectName("textEdit_Salida_Tabla_Frecuencias")
-        self.label_Texto_Tabla_fr = QtWidgets.QLabel(self.centralwidget)
-        self.label_Texto_Tabla_fr.setGeometry(QtCore.QRect(70, 190, 241, 31))
-        self.label_Texto_Tabla_fr.setObjectName("label_Texto_Tabla_fr")
-        self.label_titulo = QtWidgets.QLabel(self.centralwidget)
-        self.label_titulo.setGeometry(QtCore.QRect(290, 0, 201, 51))
-        self.label_titulo.setObjectName("label_titulo")
-        self.label_escudo_unam = QtWidgets.QLabel(self.centralwidget)
-        self.label_escudo_unam.setGeometry(QtCore.QRect(10, 10, 131, 171))
-        self.label_escudo_unam.setStyleSheet("border-image: url(:/escudoUnam/Imagenes/ImagenesSesion/unam.png);")
-        self.label_escudo_unam.setText("")
-        self.label_escudo_unam.setObjectName("label_escudo_unam")
-        self.label_escudo_fi = QtWidgets.QLabel(self.centralwidget)
-        self.label_escudo_fi.setGeometry(QtCore.QRect(660, 0, 131, 171))
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(368, 210)
+        Form.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));")
+        self.pushButton_resumir_texto = QtWidgets.QPushButton(Form)
+        self.pushButton_resumir_texto.setGeometry(QtCore.QRect(120, 60, 101, 31))
+        self.pushButton_resumir_texto.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));")
+        self.pushButton_resumir_texto.setObjectName("pushButton_resumir_texto")
+        self.pushButton_texto_url = QtWidgets.QPushButton(Form)
+        self.pushButton_texto_url.setGeometry(QtCore.QRect(120, 110, 101, 31))
+        self.pushButton_texto_url.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));")
+        self.pushButton_texto_url.setObjectName("pushButton_texto_url")
+        self.pushButton_salir = QtWidgets.QPushButton(Form)
+        self.pushButton_salir.setGeometry(QtCore.QRect(120, 160, 101, 31))
+        self.pushButton_salir.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));")
+        self.pushButton_salir.setObjectName("pushButton_salir")
+        self.label_escudo_fi = QtWidgets.QLabel(Form)
+        self.label_escudo_fi.setGeometry(QtCore.QRect(270, 10, 81, 91))
         self.label_escudo_fi.setStyleSheet("border-image: url(:/escudofi/Imagenes/InterfazPrincipal/escudo_fi_color.png);")
         self.label_escudo_fi.setText("")
         self.label_escudo_fi.setObjectName("label_escudo_fi")
-        self.textEdit_Salida_Organizacion_Valorizacion = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_Salida_Organizacion_Valorizacion.setGeometry(QtCore.QRect(420, 230, 311, 131))
-        self.textEdit_Salida_Organizacion_Valorizacion.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.textEdit_Salida_Organizacion_Valorizacion.setReadOnly(False)
-        self.textEdit_Salida_Organizacion_Valorizacion.setObjectName("textEdit_Salida_Organizacion_Valorizacion")
-        self.label_Texto_Org_Y_Valorizacion = QtWidgets.QLabel(self.centralwidget)
-        self.label_Texto_Org_Y_Valorizacion.setGeometry(QtCore.QRect(420, 190, 301, 31))
-        self.label_Texto_Org_Y_Valorizacion.setObjectName("label_Texto_Org_Y_Valorizacion")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 801, 21))
-        self.menubar.setObjectName("menubar")
-        self.menuArchivo = QtWidgets.QMenu(self.menubar)
-        self.menuArchivo.setObjectName("menuArchivo")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.menubar.addAction(self.menuArchivo.menuAction())
+        self.label_escudo_unam = QtWidgets.QLabel(Form)
+        self.label_escudo_unam.setGeometry(QtCore.QRect(0, 10, 91, 91))
+        self.label_escudo_unam.setStyleSheet("image: url(:/escudoUnam/Imagenes/ImagenesSesion/unam.png);")
+        self.label_escudo_unam.setText("")
+        self.label_escudo_unam.setObjectName("label_escudo_unam")
 
-        self.pushButton_Obtemer_Resumen.clicked.connect(self.imprime)
-        self.r1=Resumen()
+        self.pushButton_resumir_texto.clicked.connect(self.abrirVentanaPrincipal)
 
-        self.retranslateUi(MainWindow)
-        self.pushButton_Salir.clicked.connect(MainWindow.close)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(Form)
+        self.pushButton_salir.clicked.connect(Form.close)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
+    def abrirVentanaPrincipal(self):
+        self.ventana0=QtWidgets.QMainWindow()
+        #self.ui0=Ui_FormVentanaPrincipal()
+        self.ui0=Ui_MainWindow()
+        self.ui0.setupUi(self.ventana0)
+        self.ventana0.show()
 
-    def imprime(self):
-        text=self.textEdit_Entrada_de_texto.toPlainText()
-        tabla_frecuencias=self.r1.tablaFrecuencias(text)
-        oracionesYValorizacion=self.r1.oracionesYvalorizacion(tabla_frecuencias, text)
-        tabla_frec_string=self.convertirAString(tabla_frecuencias)
-        oracionesYvalorizacion_string=self.convertirAString(oracionesYValorizacion)
-        #print(string)
-        res =self.r1.resumir(text, oracionesYValorizacion)
-        self.textEdit_Salida_Resumen_Obtenido.setText(str(res))
-        self.textEdit_Salida_Tabla_Frecuencias.setText(str(tabla_frec_string))
-        self.textEdit_Salida_Organizacion_Valorizacion.setText(str(oracionesYvalorizacion_string))        
-        self.abrirDialogoAviso("Hola")
-
-    def convertirAString(self, diccionario):
-        string=""
-        for key in diccionario:
-            string+=str(key)+":"+"\t"+str(diccionario[key])+"\n"
-        return string
-
-
-    def abrirDialogoAviso(self, texto):
-        self.ventanaCam=QtWidgets.QDialog()
-        self.uiCam=Ui_DialogAviso()
-        self.uiCam.setupUi(self.ventanaCam,texto)
-        self.ventanaCam.show()
-
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton_Salir.setText(_translate("MainWindow", "Salir"))
-        self.pushButton_Obtemer_Resumen.setText(_translate("MainWindow", "Resumir texto"))
-        self.label_TextoIntroduce_Texto.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Introduce el texto a resumir:</span></p></body></html>"))
-        self.label_Resumen_Obtenido.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Resumen obtenido:</span></p></body></html>"))
-        self.label_Texto_Tabla_fr.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Tabla de frecuencias</span></p></body></html>"))
-        self.label_titulo.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:28pt; font-weight:600; color:#c8371a;\">Resumen</span></p></body></html>"))
-        self.label_Texto_Org_Y_Valorizacion.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Oraciones y Valorización</span></p></body></html>"))
-        self.menuArchivo.setTitle(_translate("MainWindow", "Archivo"))
-
-    
-
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton_resumir_texto.setText(_translate("Form", "Resumir texto"))
+        self.pushButton_texto_url.setText(_translate("Form", "Texto URL"))
+        self.pushButton_salir.setText(_translate("Form", "Salir"))
+        
 import Logos.LogoVentanaInicio.LogoEscudoFi_rc
 import Logos.LogoVentanaInicio.LogoEscudoUNAM_rc
 
@@ -143,8 +68,8 @@ import Logos.LogoVentanaInicio.LogoEscudoUNAM_rc
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
     sys.exit(app.exec_())
--- ProyectPython/version2/Interfaz/VentanaPrincipal/VentanaPrincipal_url.py
+++ ProyectPython/version2/Interfaz/VentanaPrincipal/VentanaPrincipal_url.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'VentanaPrincipal.ui'
+# Form implementation generated from reading ui file 'VentanaPrincipal_url.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
 #
 # WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 sys.path.append("..")
@@ -14,15 +12,15 @@ from Dialogos.DialogoAviso.DialogoAvisos import Ui_DialogAviso
 from Backend.Resumen import Resumen
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(801, 600)
-        MainWindow.setMinimumSize(QtCore.QSize(801, 600))
-        MainWindow.setMaximumSize(QtCore.QSize(801, 600))
-        MainWindow.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));\n"
+class Ui_MainWindow_URL(object):
+    def setupUi(self, MainWindow_URL):
+        MainWindow_URL.setObjectName("MainWindow_URL")
+        MainWindow_URL.resize(801, 600)
+        MainWindow_URL.setMinimumSize(QtCore.QSize(801, 600))
+        MainWindow_URL.setMaximumSize(QtCore.QSize(801, 600))
+        MainWindow_URL.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));\n"
 "background-color: qlineargradient(spread:pad, x1:0.824, y1:0.198864, x2:1, y2:0, stop:0 rgba(145, 214, 243, 255), stop:1 rgba(255, 255, 255, 255));")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget = QtWidgets.QWidget(MainWindow_URL)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton_Salir = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_Salir.setGeometry(QtCore.QRect(710, 530, 75, 23))
@@ -39,7 +37,7 @@ class Ui_MainWindow(object):
         self.label_Resumen_Obtenido.setGeometry(QtCore.QRect(180, 360, 241, 31))
         self.label_Resumen_Obtenido.setObjectName("label_Resumen_Obtenido")
         self.textEdit_Entrada_de_texto = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_Entrada_de_texto.setGeometry(QtCore.QRect(170, 90, 441, 91))
+        self.textEdit_Entrada_de_texto.setGeometry(QtCore.QRect(170, 90, 441, 21))
         self.textEdit_Entrada_de_texto.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.textEdit_Entrada_de_texto.setObjectName("textEdit_Entrada_de_texto")
         self.textEdit_Salida_Resumen_Obtenido = QtWidgets.QTextEdit(self.centralwidget)
@@ -76,64 +74,33 @@ class Ui_MainWindow(object):
         self.label_Texto_Org_Y_Valorizacion = QtWidgets.QLabel(self.centralwidget)
         self.label_Texto_Org_Y_Valorizacion.setGeometry(QtCore.QRect(420, 190, 301, 31))
         self.label_Texto_Org_Y_Valorizacion.setObjectName("label_Texto_Org_Y_Valorizacion")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        MainWindow_URL.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow_URL)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 801, 21))
         self.menubar.setObjectName("menubar")
         self.menuArchivo = QtWidgets.QMenu(self.menubar)
         self.menuArchivo.setObjectName("menuArchivo")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        MainWindow_URL.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow_URL)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        MainWindow_URL.setStatusBar(self.statusbar)
         self.menubar.addAction(self.menuArchivo.menuAction())
 
-        self.pushButton_Obtemer_Resumen.clicked.connect(self.imprime)
-        self.r1=Resumen()
+        self.retranslateUi(MainWindow_URL)
+        self.pushButton_Salir.clicked.connect(MainWindow_URL.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow_URL)
 
-        self.retranslateUi(MainWindow)
-        self.pushButton_Salir.clicked.connect(MainWindow.close)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-
-    def imprime(self):
-        text=self.textEdit_Entrada_de_texto.toPlainText()
-        tabla_frecuencias=self.r1.tablaFrecuencias(text)
-        oracionesYValorizacion=self.r1.oracionesYvalorizacion(tabla_frecuencias, text)
-        tabla_frec_string=self.convertirAString(tabla_frecuencias)
-        oracionesYvalorizacion_string=self.convertirAString(oracionesYValorizacion)
-        #print(string)
-        res =self.r1.resumir(text, oracionesYValorizacion)
-        self.textEdit_Salida_Resumen_Obtenido.setText(str(res))
-        self.textEdit_Salida_Tabla_Frecuencias.setText(str(tabla_frec_string))
-        self.textEdit_Salida_Organizacion_Valorizacion.setText(str(oracionesYvalorizacion_string))        
-        self.abrirDialogoAviso("Resumen listo")
-
-    def convertirAString(self, diccionario):
-        string=""
-        for key in diccionario:
-            string+=str(key)+":"+"\t"+str(diccionario[key])+"\n"
-        return string
-
-
-    def abrirDialogoAviso(self, texto):
-        self.ventanaCam=QtWidgets.QDialog()
-        self.uiCam=Ui_DialogAviso()
-        self.uiCam.setupUi(self.ventanaCam,texto)
-        self.ventanaCam.show()
-
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, MainWindow_URL):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton_Salir.setText(_translate("MainWindow", "Salir"))
-        self.pushButton_Obtemer_Resumen.setText(_translate("MainWindow", "Resumir texto"))
-        self.label_TextoIntroduce_Texto.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Introduce el texto a resumir:</span></p></body></html>"))
-        self.label_Resumen_Obtenido.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Resumen obtenido:</span></p></body></html>"))
-        self.label_Texto_Tabla_fr.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Tabla de frecuencias</span></p></body></html>"))
-        self.label_titulo.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:28pt; font-weight:600; color:#c8371a;\">Resumen</span></p></body></html>"))
-        self.label_Texto_Org_Y_Valorizacion.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Oraciones y Valorización</span></p></body></html>"))
-        self.menuArchivo.setTitle(_translate("MainWindow", "Archivo"))
-    
+        MainWindow_URL.setWindowTitle(_translate("MainWindow_URL", "Resumen por URL"))
+        self.pushButton_Salir.setText(_translate("MainWindow_URL", "Salir"))
+        self.pushButton_Obtemer_Resumen.setText(_translate("MainWindow_URL", "Resumir texto"))
+        self.label_TextoIntroduce_Texto.setText(_translate("MainWindow_URL", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Introduce un URL con texto:</span></p></body></html>"))
+        self.label_Resumen_Obtenido.setText(_translate("MainWindow_URL", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Resumen obtenido:</span></p></body></html>"))
+        self.label_Texto_Tabla_fr.setText(_translate("MainWindow_URL", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Tabla de frecuencias</span></p></body></html>"))
+        self.label_titulo.setText(_translate("MainWindow_URL", "<html><head/><body><p align=\"center\"><span style=\" font-size:28pt; font-weight:600; color:#c8371a;\">Resumen</span></p></body></html>"))
+        self.label_Texto_Org_Y_Valorizacion.setText(_translate("MainWindow_URL", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ff0000;\">Oraciones y Valorización</span></p></body></html>"))
+        self.menuArchivo.setTitle(_translate("MainWindow_URL", "Archivo"))
 
 import Logos.LogoVentanaInicio.LogoEscudoFi_rc
 import Logos.LogoVentanaInicio.LogoEscudoUNAM_rc
@@ -142,8 +109,8 @@ import Logos.LogoVentanaInicio.LogoEscudoUNAM_rc
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    MainWindow_URL = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow_URL()
+    ui.setupUi(MainWindow_URL)
+    MainWindow_URL.show()
     sys.exit(app.exec_())
